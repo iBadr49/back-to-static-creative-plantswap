0a0e9fbaa3418bc7469c54a90eaf97f61c037dcc
iBadr49 <badr.amara@hva.nl>
2023-12-14 21:30:47 +0100

    Componenten

---
+++ src/lib/componenten/Button.svelte
@@ -0,0 +1,28 @@
+<!-- Button.svelte -->
+<script>
+  export let href = "/";
+  export let label = "Button";
+</script>
+
+<a {href}>{label}</a>
+
+<style>
+  a {
+    display: inline-block;
+    padding: 10px 20px;
+    margin: 10px;
+    text-decoration: none;
+    color: black;
+    background-color: var(--button-color);
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    transition:
+      background-color 0.3s ease,
+      color 0.3s ease;
+  }
+
+  a:hover {
+    background-color: var(--background-color);
+    color: #fff;
+  }
+</style>
---
+++ src/lib/componenten/Title.svelte
@@ -0,0 +1,13 @@
+<!-- Title.svelte -->
+
+<h1>Plantswap</h1>
+
+<style>
+  h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+  }
+</style>
