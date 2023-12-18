d72387c5d4ed90df257715b689fa6de55e6bcfe3
iBadr49 <badr.amara@hva.nl>
2023-12-18 22:46:41 +0100

    Spike III
    html code !

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as Button } from '$lib/componenten/Button.svelte';
+export { default as Title } from '$lib/componenten/Title.svelte';
---
+++ src/routes/week_iii/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<section class="container">
+  <div class="text">POP</div>
+  <span>ART</span>
+  <div class="moving-background"></div>
+</section>
+
+<style>
+    
+</style>
